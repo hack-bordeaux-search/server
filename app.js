ab8536ac5106b253f6764fbcd1c298f8e465c747
Cristian Miguel Pinto Quinto <cristianmiguelpintoquinto@Cristians-MacBook-Pro-2.local>
2017-05-06 16:14:34 +0200

    new gitignore

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
 const algoliasearch = require('algoliasearch');
-var algoliaClient = algoliasearch('UKEL8J5F7R', '00eefa36b5cbcfc9eb4252e6c1c9eb87');
+var algoliaClient = algoliasearch('RMCV5125OB', '754239ab783a2df84107f3b5acee193b');
+const bodyParser = require('body-parser')
+
+const express = require('express');
+let app = express();
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
 
 const pushDataAlgolia = function(data, callback) {
     var index = algoliaClient.initIndex('alias');
     index.addObjects(data, function(err, content) {
         if (err) {
             console.error(err);
-            callback(error);
+            callback(err);
             return;
         }
+        else callback(null);
     });
-    callback(null);
+    //
 };
+
+
+
+app.post('/alias', function (req, res) {
+    if(!req.body) return res.status(500).send({message: "Body empty"});
+    if(!req.body.alias) return res.status(500).send({message: "Alias is not in body"});
+    if(!req.body.social_accounts) return res.status(500).send({message: "social_accounts is not in body"});
+    pushDataAlgolia([req.body], (err)=> {
+        if(err) return res.send(err);
+        return res.status(200).send({message: "Alias created succesfully"});
+    });
+    return;
+});
+
+app.listen(3000);
