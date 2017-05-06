be06a98082676fde37ecd931baff253400d01f17
Cristian Miguel Pinto Quinto <cristianmiguelpintoquinto@Cristians-MacBook-Pro-2.local>
2017-05-06 21:18:42 +0200

    nem integration

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 
 const express = require('express');
 let app = express();
+let nem = require("nem-sdk").default;
 
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
@@ -12,6 +13,7 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 
 
 var port = process.env.PORT || 3000;
+var private_key = process.env.PRIVATE_KEY || "nokey";
 app.listen(port);
 
 
@@ -67,24 +69,49 @@ const searchAlias = function(aliasValue, callback) {
 
 
 app.post('/alias', function (req, res) {
+    let aliasInfo = {};
     if(!req.body) return res.status(500).send({message: "Body empty"});
     if(!req.body.alias) return res.status(500).send({message: "Alias is not in body"});
+    aliasInfo.alias = req.body.alias;
     if(!req.body.social_accounts) return res.status(500).send({message: "social_accounts is not in body"});
+    aliasInfo.social_accounts = req.body.social_accounts;
 
-    searchAlias(req.body.alias, (exists)=> {
+    if(!req.body.profile || !req.body.profile.name || !req.body.profile.idnumber || !req.body.profile.country) {
+        return res.status(500).send({message: "Personal information not complete"});
+    }
+    let profile = req.body.profile;
+ 
+
+    searchAlias(aliasInfo.alias, (exists)=> {
         if(exists) return res.status(500).send({message: "The alias is not available"});
-        pushDataAlgolia(req.body, (err)=> {
+        pushDataAlgolia(aliasInfo, (err)=> {
             if(err) return res.send(err);
+             // Create an NIS endpoint object
+            var endpoint = nem.model.objects.create("endpoint")(nem.model.nodes.defaultTestnet, nem.model.nodes.defaultPort);
+
+            // Create a common object holding key
+            var common = nem.model.objects.create("common")("", "d0733ac5bbf26156280f52a8377003760b5e5b000ed5669a79a54fd01df9933a");
+
+            // Create an un-prepared transfer transaction object
+            var transferTransaction = nem.model.objects.create("transferTransaction")("TAYNFJAASOQ445STMYOWQ6IQVR2AH44AMJXDYRHC", 0, JSON.stringify(profile));
+
+            // Prepare the transfer transaction object
+            var transactionEntity = nem.model.transactions.prepare("transferTransaction")(common, transferTransaction, nem.model.network.data.testnet.id);
+
+            // Serialize transfer transaction and announce
+            nem.model.transactions.send(common, transactionEntity, endpoint);
             return res.status(200).send({message: "Alias created succesfully"});
         });
     })
     return;
 });
 
-app.put('/alias', function (req, res) {
+app.put('/alias/:id', function (req, res) {
     if(!req.body) return res.status(500).send({message: "Body empty"});
-    if(!req.body.alias) return res.status(500).send({message: "Alias is not in body"});
+    if(!req.params.id) return res.status(500).send({message: "Alias is not in body"});
     if(!req.body.social_accounts) return res.status(500).send({message: "social_accounts is not in body"});
+
+    req.body.alias = req.params.id;
 
     searchAlias(req.body.alias, (aliasElem)=> {
         if(!aliasElem) return res.status(404).send({message: "The alias not exists"});
@@ -96,3 +123,9 @@ app.put('/alias', function (req, res) {
     return;
 });
 
+app.get('/alias/:id', function (req, res) {
+    searchAlias(req.params.id, (aliasElem)=> {
+        if(!aliasElem) return res.status(200).send({available: true});
+        if(aliasElem) return res.status(200).send({available: false});
+    });
+});
